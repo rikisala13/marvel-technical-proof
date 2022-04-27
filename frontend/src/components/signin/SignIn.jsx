c017dd23a905b1fb040e96dafc54b0ce575c937f
Ricard Sala <riki_sala@hotmail.com>
2022-04-27 23:40:50 +0200

    Create login components:
    -Login
    -Logout
    -SignIn

---
+++ frontend/src/components/signin/SignIn.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import LoginButton from './Login';
+
+function SignIn() {
+  return (
+    <div className="d-flex flex-column justify-content-center mt-5">
+      <div className="d-flex flex-column align-items-center">
+        <h2> Bienvenido a Marvel Heroes App</h2>
+        <div>
+          <p>Porfavor inicia sesion para poder acceder a la pagina </p>
+        </div>
+      </div>
+      <div className="d-flex  justify-content-center mt-3">
+        <LoginButton />
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
