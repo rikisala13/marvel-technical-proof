05f69f69d21d7af375586d3d82381414e5a90453
Ricard Sala <riki_sala@hotmail.com>
2022-04-28 19:40:33 +0200

    Create comicList component

---
+++ frontend/src/components/comicList/ComicList.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import './ComicList.css';
+
+export default function ComicList({ comics }) {
+  if (comics.length > 0) {
+    return (
+      <section>
+        <div className="d-flex flex-wrap align-items-center justify-content-center">
+          {
+            comics.map((comic) => (
+              <div className="d-flex flex-column comic-item" key={comic.id}>
+                <img src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} className="list-img" alt="portada" />
+                <div>
+                  {comic.title}
+                </div>
+              </div>
+            ))
+          }
+        </div>
+
+      </section>
+    );
+  }
+  return (
+    <div>
+      <h2> Este usuario no aparece en ningun comic</h2>
+    </div>
+  );
+}
