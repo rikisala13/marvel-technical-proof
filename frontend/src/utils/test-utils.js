6f2f7d7b7818e6502ebed3357d511453818cfeb0
Ricard Sala <riki_sala@hotmail.com>
2022-04-29 16:35:58 +0200

    Create TestUtils

---
+++ frontend/src/utils/test-utils.js
@@ -0,0 +1,32 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { Provider } from 'react-redux';
+// Import your own reducer
+import { BrowserRouter } from 'react-router-dom';
+import store from '../redux/store/store';
+
+function render(
+  ui,
+  {
+    initialState,
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ children }) {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          {children}
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+// re-export everything
+export * from '@testing-library/react';
+// override render method
+export { render };
