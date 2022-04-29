b792a7e875f087a86f79bc701045445aa2636bd0
Ricard Sala <riki_sala@hotmail.com>
2022-04-29 07:14:26 +0200

    Set characters in sessionStorage
    -style hotfixes

--- frontend/src/pages/charactersList/CharactersList.jsx
+++ frontend/src/pages/charactersList/CharactersList.jsx
@@ -4,17 +4,23 @@ import { useDispatch, useSelector } from 'react-redux';
 import { loadCharacters } from '../../redux/actions/characters.creators';
 
 import CharactersGroup from '../../components/characterGroup/CharacterGroup';
+import './CharactersList.css';
 
 export default function characterList() {
   const dispatch = useDispatch();
   const characters = useSelector((store) => store.characters.characters);
+
+  // obtener los heroes del store
+  // almacenar heroes en el session Storage
+  sessionStorage.setItem('characters', JSON.stringify(characters));
+  // mandar request a la Api para pedir heroes
   useEffect(() => {
     dispatch(loadCharacters());
   }, []);
 
   if (characters.length > 0) {
     return (
-      <section data_testid="characters-list" className="container">
+      <section data_testid="characters-list" className="section__character-list">
         <CharactersGroup />
       </section>
     );
