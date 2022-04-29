7c63089d1b249e24809a10bccac4bee7fa3247ae
Ricard Sala <riki_sala@hotmail.com>
2022-04-29 11:36:28 +0200

    index.js hotfixes: -cleaning -order

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-// debuguear
-// const debug = require('debug');
-// resaltar cosas en terminal
+
 const morgan = require('morgan');
 
 require('./src/config/moongooseConfig');
@@ -10,22 +8,17 @@ require('./src/config/moongooseConfig');
 const server = express();
 const port = process.env.PORT || 5001;
 
-server.use(express.json);
-
-server.use(express.json());
 server.use(morgan('dev'));
+server.use(express.json());
 
-server.get('/', (request, response) => {
-  response.send('<h1>hello world</h1>');
-});
 // Rutas
 const charactersRouter = require('./src/routes/charactersRoute');
 
 // path de las rutas
-server.use('/characters', charactersRouter);
+server.use('/', charactersRouter);
 
 // mensaje que se va a mostrar por consola cuando se levante el servidor
 server.listen(
   port,
-  () => console.log(`Server is running on http://localhost${port}`)
+  () => console.log(`Server is running on http://localhost:${port}`)
 );
