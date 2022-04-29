6c479840203570da3b322d1c28bcc0e12cbe6133
Ricard Sala <riki_sala@hotmail.com>
2022-04-29 11:32:02 +0200

    Add methods in rotues

--- Backend/src/routes/charactersRoute.js
+++ Backend/src/routes/charactersRoute.js
@@ -1,12 +1,25 @@
 const { Router } = require('express');
 const {
-  getAllCharacters
+  getAllCharacters,
+  postCharacter,
+  findOneCharacter,
+  getOneCharacter,
+  putOneCharacter,
+  deleteOneCharacter
 } = require('../controllers/characters.controller');
 
 const productsRouter = Router();
 
 productsRouter
   .route('/')
-  .get(getAllCharacters);
+  .get(getAllCharacters)
+  .post(postCharacter);
+
+productsRouter
+  .route('/:characterId')
+  .all(findOneCharacter)
+  .get(getOneCharacter)
+  .put(putOneCharacter)
+  .delete(deleteOneCharacter);
 
 module.exports = productsRouter;
