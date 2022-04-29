bb9017b2874605652329aa461aee8b0d51a5fb9e
Ricard Sala <riki_sala@hotmail.com>
2022-04-29 07:06:29 +0200

    Clean sessionstorage when you log out

--- frontend/src/components/signin/Logout.jsx
+++ frontend/src/components/signin/Logout.jsx
@@ -1,10 +1,17 @@
+/* eslint-disable max-len */
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
 
 function LogoutButton() {
-  const { logout } = useAuth0();
+  const navigate = useNavigate();
+  function redirect() {
+    sessionStorage.clear();
+    document.cookie = 'auth0.ujb8XDvfdTk8W76Q0oySrKNqSdw3R2hF.is.authenticated=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    document.cookie = '_legacy_auth0.ujb8XDvfdTk8W76Q0oySrKNqSdw3R2hF.is.authenticated; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    navigate('/login');
+  }
   return (
-    <button type="button" className="btn btn-dark" onClick={(() => logout({ returnTo: window.location.origin }), sessionStorage.clear())}>LogOut</button>
+    <button type="button" className="btn btn-dark" onClick={redirect}>LogOut</button>
 
   );
 }
