0758bc0119e4263762350e7b98d7a411f2c6d85b
Ricard Sala <riki_sala@hotmail.com>
2022-04-29 11:32:47 +0200

    Change of model for a more generic one

--- Backend/src/models/character.model.js
+++ Backend/src/models/character.model.js
@@ -1,53 +1,26 @@
 const mongoose = require('mongoose');
 
-const characterSchema = mongoose.Schema({
-
-  comics: {
-    available: Number,
-    collectionURI: String,
-    items: [{
-      name: String,
-      resourceURI: String
-    }]
+const completeCharacterSchema = mongoose.Schema({
+  id: String,
+  user: {
+    name: String,
+    picture: String,
+    email: String
   },
-  description: String,
-  events: {
-    available: Number,
-    collectionURI: String,
-    items: [{
-      name: String,
-      resourceURI: String
-    }]
-  },
-  id: Number,
-  modified: String,
-  name: String,
-  resourceURI: String,
-  series: {
-    available: Number,
-    collectionURI: String,
-    items: [{
-      name: String,
-      resourceURI: String
-    }]
-  },
-  stories: {
-    available: Number,
-    collectionURI: String,
-    items: [{
-      name: String,
-      resourceURI: String
-    }]
-  },
-  thumbnail: {
-    path: String,
-    extension: String
-  },
-  urls: [{
-    type: String,
-    url: String
-  }]
-
+  character: {
+    id: Number,
+    name: String,
+    thumbnail: {
+      extension: String,
+      path: String
+    },
+    like: Boolean,
+    score: Number,
+    coments: [
+      { String }
+    ],
+    totalScore: Number
+  }
 });
 
-module.exports = mongoose.model('Character', characterSchema);
+module.exports = mongoose.model('CompleteCharacter', completeCharacterSchema);
